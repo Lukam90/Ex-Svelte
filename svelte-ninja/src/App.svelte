cd61a1d24334a2a31286b12a6ce28f07dd44435a
Lucien HAMM <lucien.hamm@gmail.com>
2024-04-29 15:46:58 +0200

    MAJ Ex Svelte > Forms

--- svelte-ninja/src/App.svelte
+++ svelte-ninja/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 import Modal from "./Modal.svelte";
+import AddPersonForm from "./AddPersonForm.svelte";
 
 let showModal = false;
 
@@ -21,11 +22,7 @@ const toggleModal = () => {
 <Modal message="Hey, I'm a prop value!" {showModal} on:click={toggleModal}>
 	<h3>Add a New Person</h3>
 
-	<form>
-		<input type="text" placeholder="name" />
-		<input type="text" placeholder="belt colour" />
-		<button>Add Person</button>
-	</form>
+	
 
 	<div slot="title">
 		<h3>Add a New Person</h3>
