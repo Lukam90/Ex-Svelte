b0fce4339155043a3d1c366f25af03cefd100bc7
Lucien HAMM <lucien.hamm@gmail.com>
2024-04-29 14:59:17 +0200

    MAJ Ex Svelte > Components

--- svelte-ninja/src/App.svelte
+++ svelte-ninja/src/App.svelte
@@ -1,29 +1,43 @@
 <script>
-	let firstName = "Jimi";
-	let lastName = "Hendrix";
-	let beltColour = "black";
+import Modal from "./Modal.svelte";
 
-	$: fullName = `${firstName} ${lastName}`;
+let showModal = false;
 
-	const handleClick = () => {
-		beltColour = "orange";
-	};
+let people = [
+	{ id: 1, name: "yoshi", beltColour: "black", age: 25 },
+	{ id: 2, name: "mario", beltColour: "orange", age: 45 },
+	{ id: 3, name: "luigi", beltColour: "brown", age: 35 }
+]
 
-	const handleInput = (e) => {
-		beltColour = e.target.value;
-	};
+const handleClick = (id) => {
+	people = people.filter((person) => person.id != id)
+};
+
+const toggleModal = () => {
+	showModal = ! showModal;
+};
 </script>
 
+<Modal message="Hey, I'm a prop value!" {showModal} on:click={toggleModal} />
+
 <main>
-	<p>{fullName} - {beltColour}</p>
+	<button on:click|once={toggleModal}>Open Modal</button>
 
-	<p style="color: {beltColour}">{beltColour} belt</p>
+	{#each people as person (person.id)}
+	<div>
+		<h4>{person.name}</h4>
 
-	<button on:click={handleClick}>update belt color</button>
+		{#if person.beltColour === "black"}
+		<p><strong>MASTER</strong></p>
+		{/if}
 
-	<input type="text" bind:value={firstName} />
-	<input type="text" bind:value={lastName} />
-	<input type="text" bind:value={beltColour} />
+		<p>{person.age} years old, {person.beltColour} belt</p>
+
+		<button on:click={handleClick(person.id)}>delete</button>
+	</div>
+	{:else}
+		<p>There are no people to show.</p>
+	{/each}
 </main>
 
 <style>
@@ -32,13 +46,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
